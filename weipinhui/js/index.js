865d1cd0dab90afeca168563e2d03f8b952c9b88
zheng <936162702@qq.com>
2022-10-21 23:54:24 +0800

    保存

---
+++ weipinhui/js/index.js
@@ -0,0 +1,15 @@
+// var options = {
+//     animateClass: 'animate__animated',
+//     animateThreshold: 100,
+//     scrollPollInterval: 20
+// }
+// $('.aniview').AniView(options);
+
+$(function () {
+    // $('.title-left').mouseenter(function () {
+    //     $('.title-dw').slideDown(2000)
+    // })
+
+    
+})
+
